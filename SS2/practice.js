5e3bc76c96504e747f7eaa81104d6ee7c00107ea
khanhhlq <lee.quockhanh1107@gmail.com>
2026-09-13 18:00:50 +0700

    done ss2

---
+++ SS2/practice.js
@@ -0,0 +1,26 @@
+// Bài 2:
+var text_1 = "Hello";
+var text_2 = "World!"
+
+console.log(text_1 + " " + text_2)
+
+// Bài 1:
+var text = "lvv jsb04";
+var result = "";
+
+// Hàm viết hoa: toUpperCase
+// Vòng lặp
+// Điều kiện
+
+for (let i = 0; i <= text.length; i++){
+    if (i == 0){
+        result = result + text[0].toUpperCase();
+    }
+    else if (text[i - 1] == " "){
+        result = result + text[i].toUpperCase();
+    }
+    else{
+        result = result + text[i];
+    }
+}
+console.log(result)
