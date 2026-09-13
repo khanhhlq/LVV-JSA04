ca5a818769d0753b8dd5a0c29e4639c8b1b18d84
khanhhlq <lee.quockhanh1107@gmail.com>
2026-09-13 17:39:56 +0700

    done ss2

---
+++ SS2/app.js
@@ -0,0 +1,47 @@
+// Thực hành khai báo biến như sau
+// Tên biến: winners, name, isEligible
+// Với giá trị lần lượt là 2, "Duke" và false
+// Bài làm:
+let winners = 2; // Number
+let name = "Duke"; // String
+let isEligible = false // Boolean
+
+// console.log để in ra
+console.log(winners)
+console.log(name)
+console.log(isEligible)
+
+// Camelcase
+var firstName = "Khanh"
+var lastName = "Le"
+var yourEmail = "example@gmail.com"
+var dateCreated = "11/09/2026"
+var dateUpdated = "12/09/2026"
+var dateDeleted = "13/09/2026"
+
+// Gộp chuỗi
+var str1 = "Hello ";
+var str2 = "World";
+var str = str1 + str2;
+console.log(str)
+
+// Length (Độ dài chuỗi)
+console.log(str1.length)
+console.log(str2.length)
+
+// Split (Tách chuỗi)
+let data = "apple,banana,orange";
+let subString = data.split(",")
+console.log(subString)
+
+// Viết hoa và thường của chuỗi
+var text_1 = "JSA04"
+// Viết thường
+console.log(text_1.toLowerCase())
+var text_2 = "lvv"
+// Viết hoa
+console.log(text_2.toUpperCase())
+
+// Xóa khoảng trắng đầu và cuối
+var region = "    VIET NAM     "
+console.log(region.trim())
